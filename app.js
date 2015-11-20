35b7af75d4e6e52d65dfe878172b63dc417705db
Daniel <udevelopd@gmail.com>
2015-11-20 11:55:47 -0600

    Function expressions

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
-var a, b = 2;
-var c = a + b;
+// function statement
+function greet() {
+    console.log('Hi!');
+}
+greet();
 
-console.log(c);
+// Functions are first-class
+function logGretting(fn) {
+    fn();
+}
+logGretting(greet);
+
+// Function expression and this still first-class
+var greetMe = function () {
+    console.log('Hi again!');
+};
+greetMe();
+// it's first-class
+logGretting(greetMe);
+
+// Use a function expression on the fly
+logGretting(function(){
+    console.log('Hi on the fly!');
+});
