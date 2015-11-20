fe0b8ac4fb066d42b363f9053d886d6bab8680ab
Daniel <udevelopd@gmail.com>
2015-11-20 12:19:08 -0600

    Objects literals

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 var greet = require('./greet');
 
 greet();
+
+
+// Objects Literals
+
+var person = {
+    firtsName: 'Dan',
+    lastName: 'Garcia',
+    greeting: function(){
+        console.log("Hi " + this.firtsName + " " + this.lastName + "!");
+    }
+};
+
+person.greeting();
