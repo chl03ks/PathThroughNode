b0d6737f1209a8b015e78cdbc7bb9bc67b5e19da
Daniel <udevelopd@gmail.com>
2015-11-25 11:29:24 -0600

    Objects properties, Fist Class Functions and Arrays

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
-var util = require('util');
+// object properties and methods
 
-var name = "Tony";
-var greet = util.format("Hello, %s", name);
+var obj = {
+    greet: 'hello',
+};
 
-util.log(greet);
+console.log(obj.greet);
+
+
+// functions and arrays
+
+var arr = [];
+
+arr.push(function () {
+    console.log("Hello 1");
+});
+arr.push(function () {
+    console.log("Hello 2");
+});
+arr.push(function () {
+    console.log("Hello 3");
+});
+
+arr.forEach(function (item) {
+    item();
+});
