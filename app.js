cc530f6a03504316bb156e0153a22d1f7277774a
Daniel <udevelopd@gmail.com>
2015-11-26 19:07:27 -0600

    call and apply

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-var name = "Dan";
-var greet  = "Hello " + name;
+var obj = {
+    name: "Daniel",
+    greet: function () {
+            console.log(`Hello ${this.name}`);
+    },
+};
 
-var greet2 = `Hello ${ name }`;
-
-console.log(greet2);
-console.log(greet);
+obj.greet();
+obj.greet.call({name:'Ulises Daniel'});
+obj.greet.apply({name:'Ulises Daniel'});
