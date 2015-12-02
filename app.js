4bfc280256a674df9cad6c32c1ad7b71270e33d3
Daniel <udevelopd@gmail.com>
2015-12-02 14:26:18 -0600

    Buffers

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-var Greeter = require('./greeter');
+var buf = new Buffer('Hello World', 'utf8');
+console.log(buf);
+console.log(buf.toString());
+console.log(buf.toJSON());
+console.log(buf[2]);
 
-var  greeter = new  Greeter();
+buf.write('wo');
+console.log(buf);
+console.log(buf.toString());
 
-greeter.on('greet', function (data) {
-    console.log('Some one Greeted! ' + data);
-});
-
-greeter.greet('Tony');
+/* its good to know what buffers are */
