24a054de87c041bf734c17ce5df8c308361ec83d
Daniel <udevelopd@gmail.com>
2015-12-07 10:11:15 -0600

    Pipes and gzip

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var fs = require('fs');
+var zlib = require('zlib');
 
-var readable = fs.createReadStream(__dirname +
-'/greet.txt', { encoding: 'utf8', highWatterMark: 16 * 1024 } );
+var readable = fs.createReadStream(__dirname + '/greet.txt' );
 
 var writable = fs.createWriteStream(__dirname +'/greetcopy.txt');
 
-readable.on('data', function (chunk) {
-    console.log(chunk);
-    writable.write(chunk);
-});
+var compress =  fs.createWriteStream(__dirname +'/greetcopy.txt.gz');
+
+var gzip = zlib.createGzip();
+
+readable.pipe(writable);
+readable.pipe(gzip).pipe(compress);
