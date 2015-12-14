c0674cc82046d7b5d4b9db1e1991f9cff18614b2
Daniel <udevelopd@gmail.com>
2015-12-13 21:57:28 -0600

    working with the full stack and backend and front end

--- app.js
+++ app.js
@@ -1,45 +1,29 @@
 'use strcit';
 var express = require('express');
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-
-mongoose.connect('mongodb://test:test@ds027335.mongolab.com:27335/addresbook');
-
-var Schema = mongoose.Schema;
-
-var personSchema = new Schema({
-    firstname: String,
-    lastname: String,
-    address: String,
-});
-
-var person = mongoose.model('Person', personSchema);
-
-var jonh = person({
-    firstname: 'John',
-    lastname: 'Doe',
-    address: '55 Main St.'
-});
-
-jonh.save(function (err) {
-    if(err) throw err;
-    console.log('Person Save');
-});
 
 var app = express();
 var port = process.env.PORT || 3000;
 
+var people = [
+    {
+        name: 'Dan Garcia'
+    },
+    {
+        name: 'Rick Garcia'
+    },
+    {
+        name: 'Joe Garcia'
+    },
+];
+
 app.set('view engine', 'jade');
+app.use('/assets', express.static( __dirname + '/public'));
+app.use('/bower', express.static( __dirname + '/bower_components'));
 
 app.get('/', function (req, res) {
 
-    person.find({}, function (err, users) {
-        if (err) throw err;
-        console.log(users);
-    });
-    res.render('index');
+    res.render('index', {serverPeople: people});
 
 });
-
 
 app.listen(port);
